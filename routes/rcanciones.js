0406a93b6e6f7e1eac59dd58d434ef98302a8864
Tomás Argüelles <nekuyshiki@gmail.com>
2021-04-12 11:32:18 +0200

    SDI-2021-605-9.2-Redirección de peticiones.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -131,7 +131,7 @@ module.exports = function(app, swig, gestorBD) {
                     if( result == null){
                         res.send("Error en la modificación");
                     } else {
-                        res.send("Modificado");
+                        res.redirect("/publicaciones");
                     }
                 });
             }
@@ -192,7 +192,7 @@ module.exports = function(app, swig, gestorBD) {
                                     if (err) {
                                         res.send("Error al subir el audio");
                                     } else {
-                                        res.send("Agregada id: "+ id);
+                                        res.redirect("/publicaciones");
                                     }
                                 });
                             }
