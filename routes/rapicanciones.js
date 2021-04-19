5068c2c6b8bdca7fb4001859814c1d80fd006b6b
Tomás Argüelles <nekuyshiki@gmail.com>
2021-04-19 19:19:30 +0200

    SDI-2021-605-sesión10-Ejercicio-complementario1

--- routes/rapicanciones.js
+++ routes/rapicanciones.js
@@ -1,123 +1,158 @@
-module.exports = function(app, gestorBD) {
+module.exports = function (app, gestorBD) {
     //GET
-    app.get("/api/cancion", function(req, res) {
-        gestorBD.obtenerCanciones( {} , function(canciones) {
+    app.get("/api/cancion", function (req, res) {
+        gestorBD.obtenerCanciones({}, function (canciones) {
             if (canciones == null) {
                 res.status(500);
                 res.json({
-                    error : "se ha producido un error"
+                    error: "se ha producido un error"
                 })
             } else {
                 res.status(200);
-                res.send( JSON.stringify(canciones) );
+                res.send(JSON.stringify(canciones));
             }
         });
     });
 
-    app.get("/api/cancion/:id", function(req, res) {
-        let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)}
-        gestorBD.obtenerCanciones(criterio,function(canciones){
-            if ( canciones == null ){
+    app.get("/api/cancion/:id", function (req, res) {
+        let criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)}
+        gestorBD.obtenerCanciones(criterio, function (canciones) {
+            if (canciones == null) {
                 res.status(500);
                 res.json({
-                    error : "se ha producido un error"
+                    error: "se ha producido un error"
                 })
             } else {
                 res.status(200);
-                res.send( JSON.stringify(canciones[0]) );
+                res.send(JSON.stringify(canciones[0]));
             }
         });
     });
 
     //DELETE
-    app.delete("/api/cancion/:id", function(req, res) {
-        let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)}
-        gestorBD.eliminarCancion(criterio,function(canciones){
-            if ( canciones == null ){
-                res.status(500);
+    app.delete("/api/cancion/:id", function (req, res) {
+        let criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)}
+        validaDatosCancion(cancion, function (errors) {
+            if (errors !== null && errors.length > 0) {
+                res.status(403);
                 res.json({
-                    error : "se ha producido un error"
+                    errores: errors
                 })
             } else {
-                res.status(200);
-                res.send( JSON.stringify(canciones) );
+                gestorBD.eliminarCancion(criterio, function (canciones) {
+                    if (canciones == null) {
+                        res.status(500);
+                        res.json({
+                            error: "se ha producido un error"
+                        })
+                    } else {
+                        res.status(200);
+                        res.send(JSON.stringify(canciones));
+                    }
+                });
             }
         });
     });
 
     //POST
-    app.post("/api/cancion", function(req, res) {
+    app.post("/api/cancion", function (req, res) {
         let cancion = {
-            nombre : req.body.nombre,
-            genero : req.body.genero,
-            precio : req.body.precio,
+            nombre: req.body.nombre,
+            genero: req.body.genero,
+            precio: req.body.precio,
         }
 // ¿Validar nombre, genero, precio?
-        gestorBD.insertarCancion(cancion, function(id){
-            if (id == null) {
-                res.status(500);
+        validaDatosCancion(cancion, function (errors) {
+            if (errors !== null && errors.length > 0) {
+                res.status(403);
                 res.json({
-                    error : "se ha producido un error"
+                    errores: errors
                 })
             } else {
-                res.status(201);
-                res.json({
-                    mensaje : "canción insertada",
-                    _id : id
-                })
+                gestorBD.insertarCancion(cancion, function (id) {
+                    if (id == null) {
+                        res.status(500);
+                        res.json({
+                            error: "se ha producido un error"
+                        })
+                    } else {
+                        res.status(201);
+                        res.json({
+                            mensaje: "canción insertada",
+                            _id: id
+                        })
+                    }
+                });
             }
         });
     });
 
-    app.post("/api/autenticar/", function(req, res) {
+    app.post("/api/autenticar/", function (req, res) {
         let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
             .update(req.body.password).digest('hex');
         let criterio = {
-            email : req.body.email,
-            password : seguro
+            email: req.body.email,
+            password: seguro
         }
-        gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
             if (usuarios === null || usuarios.length === 0) {
                 res.status(401);
                 res.json({
-                    autenticado : false
+                    autenticado: false
                 })
             } else {
                 let token = app.get('jwt').sign(
-                    {usuario: criterio.email , tiempo: Date.now()/1000},
+                    {usuario: criterio.email, tiempo: Date.now() / 1000},
                     "secreto");
                 res.status(200);
                 res.json({
                     autenticado: true,
-                    token : token
+                    token: token
                 });
             }
         });
     });
 
     //PUT
-    app.put("/api/cancion/:id", function(req, res) {
-        let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
+    app.put("/api/cancion/:id", function (req, res) {
+        let criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
         let cancion = {}; // Solo los atributos a modificar
-        if ( req.body.nombre != null)
-            cancion.nombre = req.body.nombre;
-        if ( req.body.genero != null)
-            cancion.genero = req.body.genero;
-        if ( req.body.precio != null)
-            cancion.precio = req.body.precio;
-        gestorBD.modificarCancion(criterio, cancion, function(result) {
-            if (result == null) {
-                res.status(500);
+        validaDatosCancion(cancion, function (errors) {
+            if (errors !== null && errors.length > 0) {
+                res.status(403);
                 res.json({
-                    error : "se ha producido un error"
+                    errores: errors
                 })
             } else {
-                res.status(200);
-                res.json({
-                    mensaje : "canción modificada",
-                    _id : req.params.id
-                })
+                gestorBD.modificarCancion(criterio, cancion, function (result) {
+                    if (result == null) {
+                        res.status(500);
+                        res.json({
+                            error: "se ha producido un error"
+                        })
+                    } else {
+                        res.status(200);
+                        res.json({
+                            mensaje: "canción modificada",
+                            _id: req.params.id
+                        })
+                    }
+                });
             }
         });
     })
+
+    function validaDatosCancion(cancion, funcionCallback) {
+        let errors = [];
+        if (cancion.nombre === null || typeof cancion.nombre === 'undefinded' || cancion.nombre === "")
+            errors.push("El nombre de la canción no puede estar vacio");
+        if (cancion.genero === null || typeof cancion.genero === 'undefinded' || cancion.genero === "")
+            errors.push("El genero de la canción no puede estar vacio");
+        if (cancion.precio === null || typeof cancion.precio === 'undefinded' || cancion.precio === "")
+            errors.push("El precio de la canción no puede estar vacio");
+        if (errors.length <= 0)
+            funcionCallback(null)
+        else
+            funcionCallback(errors)
+    }
 }
